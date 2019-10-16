0a8d7b4c54b7ffa18c1a924f7cfb82ac72b47ff8
JiazeWang <jiazewang@csu.edu.cn>
2019-10-16 13:28:58 +0800

    1

--- infer0.py
+++ infer0.py
@@ -46,42 +46,41 @@ def main():
 
     with torch.no_grad():
 
-        for name, root in to_test.iteritems():
+        #for name, root in to_test.iteritems():
+        root = '/mnt/SSD/jzwang/frames/shot_used/tt0078841/shot_0699.mp4'
+        precision_record, recall_record, = [AvgMeter() for _ in range(256)], [AvgMeter() for _ in range(256)]
+        mae_record = AvgMeter()
 
-            precision_record, recall_record, = [AvgMeter() for _ in range(256)], [AvgMeter() for _ in range(256)]
-            mae_record = AvgMeter()
+        if args['save_results']:
+            check_mkdir(os.path.join(ckpt_path, exp_name, '(%s)_%s' % (exp_name, args['snapshot'])))
+        img_list = [os.path.splitext(f)[0] for f in os.listdir(root) if f.endswith('.jpg')]
+        for idx, img_name in enumerate(img_list):
+            print 'predicting for  %d / %d' % ( idx + 1, len(img_list))
 
+            img = Image.open(os.path.join(root, img_name + '.jpg')).convert('RGB')
+            img_var = Variable(img_transform(img).unsqueeze(0), volatile=True).cuda()
+            prediction = net(img_var)
+            prediction = np.array(to_pil(prediction.data.squeeze(0).cpu()))
+
+            if args['crf_refine']:
+                prediction = crf_refine(np.array(img), prediction)
+            """
+            gt = np.array(Image.open(os.path.join(root, img_name + '.png')).convert('L'))
+            precision, recall, mae = cal_precision_recall_mae(prediction, gt)
+            for pidx, pdata in enumerate(zip(precision, recall)):
+                p, r = pdata
+                precision_record[pidx].update(p)
+                recall_record[pidx].update(r)
+            mae_record.update(mae)
+            """
             if args['save_results']:
-                check_mkdir(os.path.join(ckpt_path, exp_name, '(%s) %s_%s' % (exp_name, name, args['snapshot'])))
-            root = '/mnt/SSD/jzwang/frames/shot_used/tt0078841/shot_0699.mp4'
-            img_list = [os.path.splitext(f)[0] for f in os.listdir(root) if f.endswith('.jpg')]
-            for idx, img_name in enumerate(img_list):
-                print 'predicting for %s: %d / %d' % (name, idx + 1, len(img_list))
+                Image.fromarray(prediction).save(os.path.join(ckpt_path, exp_name, '(%s)_%s' % (
+                    exp_name, args['snapshot']), img_name + '.png'))
+"""
+        fmeasure = cal_fmeasure([precord.avg for precord in precision_record],
+                                [rrecord.avg for rrecord in recall_record])
 
-                img = Image.open(os.path.join(root, img_name + '.jpg')).convert('RGB')
-                img_var = Variable(img_transform(img).unsqueeze(0), volatile=True).cuda()
-                prediction = net(img_var)
-                prediction = np.array(to_pil(prediction.data.squeeze(0).cpu()))
-
-                if args['crf_refine']:
-                    prediction = crf_refine(np.array(img), prediction)
-                """
-                gt = np.array(Image.open(os.path.join(root, img_name + '.png')).convert('L'))
-                precision, recall, mae = cal_precision_recall_mae(prediction, gt)
-                for pidx, pdata in enumerate(zip(precision, recall)):
-                    p, r = pdata
-                    precision_record[pidx].update(p)
-                    recall_record[pidx].update(r)
-                mae_record.update(mae)
-                """
-                if args['save_results']:
-                    Image.fromarray(prediction).save(os.path.join(ckpt_path, exp_name, '(%s) %s_%s' % (
-                        exp_name, name, args['snapshot']), img_name + '.png'))
-    """
-            fmeasure = cal_fmeasure([precord.avg for precord in precision_record],
-                                    [rrecord.avg for rrecord in recall_record])
-
-            results[name] = {'fmeasure': fmeasure, 'mae': mae_record.avg}
+        results[name] = {'fmeasure': fmeasure, 'mae': mae_record.avg}
 
     print 'test results:'
     print results

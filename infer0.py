06723d3e7ed1afcd10983ef7fcb65522446d49ad
JiazeWang <jiazewang@csu.edu.cn>
2019-10-16 15:02:56 +0800

    1

--- infer0.py
+++ infer0.py
@@ -27,6 +27,7 @@ args = {
 }
 
 img_transform = transforms.Compose([
+    transforms.resize(300)
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])

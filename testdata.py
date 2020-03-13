027ce543f1fa7149cd0fbe2a3d36d1df40a7812b
JiazeWang <jiazewang@csu.edu.cn>
2020-03-14 00:39:04 +0800

    fix

--- testdata.py
+++ testdata.py
@@ -1,3 +1,4 @@
+import os
 filename_input = "sense_train.txt"
 with open(filename_input) as f:
     lines = f.readlines()
@@ -12,7 +13,7 @@ def make_dataset():
     ground = []
     for line in folders:
         img_list = [os.path.splitext(f)[0] for f in os.listdir(line) if f.startswith('frame_')]
-        line_train, line_ground = [(os.path.join(root, img_name + '.png'), os.path.join(root[0:-6], 'mask', img_name + '.png')) for img_name in img_list]
+        line_train, line_ground = [(os.path.join(line, img_name + '.png'), os.path.join(line[0:-6], 'mask', img_name + '.png')) for img_name in img_list]
         img.append(img_list)
         ground.appen(line_ground)
     return img, ground

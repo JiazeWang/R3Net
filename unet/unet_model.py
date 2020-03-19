4968f6ea2a9c2cf3e63e6e09dd85fb6a0a13944e
JiazeWang <jiazewang@csu.edu.cn>
2020-03-19 23:10:47 +0800

    fix

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -12,16 +12,16 @@ class UNet(nn.Module):
         self.n_classes = 1
         self.bilinear = True
 
-        self.inc = DoubleConv(n_channels, 64)
+        self.inc = DoubleConv(self.n_channels, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
         self.down4 = Down(512, 512)
-        self.up1 = Up(1024, 256, bilinear)
-        self.up2 = Up(512, 128, bilinear)
-        self.up3 = Up(256, 64, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        self.up1 = Up(1024, 256, self.bilinear)
+        self.up2 = Up(512, 128, self.bilinear)
+        self.up3 = Up(256, 64, self.bilinear)
+        self.up4 = Up(128, 64, self.bilinear)
+        self.outc = OutConv(64, self.n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)

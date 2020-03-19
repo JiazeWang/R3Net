5a9e36265f580830fef520e5f7335248d1635ab9
JiazeWang <jiazewang@csu.edu.cn>
2020-03-19 23:27:03 +0800

    fix

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -9,7 +9,7 @@ class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
     def __init__(self, in_channels, out_channels):
-        super().__init__()
+        super(DoubleConv, self).__init__()
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
